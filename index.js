6641965901811bd4e6b40589291ee012cd708e91
Shamilxmhd <shamilxmuhd@gmail.com>
2024-02-15 10:42:32 +0530

    studentReg-frontend

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const router = require ('./Routes/routes')
-// path
-const path = require ('path')
+
 const afServer = express()
 require('./DB/connection')
 
@@ -13,12 +12,7 @@ afServer.use(router)
 
 
 
-afServer.use(express.static(path.join(__dirname, 'client/build')));
 
-afServer.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-  
 
 
 const PORT = 3000 || process.env.PORT
